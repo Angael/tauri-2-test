3de1c167b519972f00470b36f6b66f00ce6be9ab
Angael <krzysztofwidacki@gmail.com>
2025-06-21 15:17:42 +0200

    Remove test module and clean up main function in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// When testing
-// mod test;
-
 fn main() {
-    // When testing
-    // test::run();
-
-    // When developing
     tauri_2_test_lib::run()
 }
