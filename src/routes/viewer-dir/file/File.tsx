8e6591f635dc69a0b02c18025c96ac210eaa99a5
Angael <krzysztofwidacki@gmail.com>
2025-07-05 14:23:38 +0200

    feat: integrate FilePlaceholder component for better thumbnail handling

--- src/routes/viewer-dir/file/File.tsx
+++ src/routes/viewer-dir/file/File.tsx
@@ -3,6 +3,7 @@ import { convertFileSrc } from "@tauri-apps/api/core";
 import { memo, useEffect, useRef } from "react";
 import { DirWithFiles } from "../../saved-folders/FilesInDirs.type";
 import css from "./File.module.css";
+import FilePlaceholder from "./FilePlaceholder";
 // import { listen } from "@tauri-apps/api/event";
 
 type Props = {
@@ -11,34 +12,37 @@ type Props = {
 };
 
 const TILE_SIZE = 256; // px
-const tile_rows_and_cols = 4; // 3 rows and 3 columns
 
 const File = ({ dir: _, file }: Props) => {
   const { ref, inViewport } = useInViewport();
 
   const imgRef = useRef<HTMLImageElement>(null);
+  const hasThumbnail = file.thumbs.length > 0;
 
   useEffect(() => {
-    // TODO: hack, change later
-    if (!file.video_stats) {
+    const thumbsWithGrid = file.thumbs.find((thumb) => thumb.grid);
+    if (!thumbsWithGrid || !thumbsWithGrid.grid) {
       return;
     }
+
+    const [columns, rows] = thumbsWithGrid.grid;
+    const totalTiles = columns * rows;
+    
     let intervalRef = null as any;
     let currentTile = 0;
 
     const updateTilePosition = () => {
       if (!imgRef.current) return;
 
-      const row = Math.floor(currentTile / tile_rows_and_cols);
-      const col = currentTile % tile_rows_and_cols;
+      const row = Math.floor(currentTile / columns);
+      const col = currentTile % columns;
 
       const xOffset = -col * TILE_SIZE;
       const yOffset = -row * TILE_SIZE;
 
       imgRef.current.style.objectPosition = `${xOffset}px ${yOffset}px`;
 
-      currentTile =
-        (currentTile + 1) % (tile_rows_and_cols * tile_rows_and_cols);
+      currentTile = (currentTile + 1) % totalTiles;
     };
 
     intervalRef = setInterval(updateTilePosition, 150);
@@ -48,7 +52,7 @@ const File = ({ dir: _, file }: Props) => {
         clearInterval(intervalRef);
       }
     };
-  }, []);
+  }, [file.thumbs]);
 
   // const src = useMemo(() => {
   //   // TODO: works only on Windows, and bad performance?
@@ -81,12 +85,15 @@ const File = ({ dir: _, file }: Props) => {
     `C:\\Users\\krzys\\AppData\\Local\\com.tauri-2-test.app\\files\\${file.id}\\thumbnail.avif`
   );
 
+  const content = hasThumbnail ? (
+    <img ref={imgRef} className={css.thumbnail} src={src} alt={file.name} />
+  ) : (
+    <FilePlaceholder file={file} />
+  );
+
   return (
     <div ref={ref} className={css.fileWrapper}>
-      {/* {inViewport && <FilePlaceholder file={file} />} */}
-      {inViewport && (
-        <img ref={imgRef} className={css.thumbnail} src={src} alt={file.name} />
-      )}
+      {inViewport && content}
     </div>
   );
 };
