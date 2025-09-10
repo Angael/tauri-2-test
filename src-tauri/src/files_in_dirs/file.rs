ebf5d505b0b5c4d1282f36e584369f6fb0151af1
Angael <krzysztofwidacki@gmail.com>
2025-09-10 10:06:22 +0200

    unused code

--- src-tauri/src/files_in_dirs/file.rs
+++ src-tauri/src/files_in_dirs/file.rs
@@ -3,21 +3,6 @@ use serde::{Deserialize, Serialize};
 use crate::thumb_gen::thumbnail::Thumbnail;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct FileThumbs {
-    // Static thumbnails
-    pub s256: bool,
-    pub s512: bool,
-
-    // Tiled thumbnails
-    pub t256: bool,
-    pub t512: bool,
-
-    // Animated thumbnails
-    pub a256: bool,
-    pub a512: bool,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct File {
     pub id: String, // nanoid
     pub name: String,
